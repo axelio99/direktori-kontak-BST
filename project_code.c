62d71c7ac5c699a8409d6a3f0af62ba1d7275e92
axelio99 <xxgamm7@gmail.com>
2026-05-25 22:40:07 +0700

    bikin fungsi create node

--- project_code.c
+++ project_code.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct node
+{
+    char nama[15];
+    long long int no_telp;
+    struct node* left;
+    struct node* right;
+};
+
+struct node* root = NULL;
+
+struct node* CreateNode(char nama[15], long long int no_telp)
+{
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    strcpy(newNode->nama, nama);
+    newNode->no_telp = no_telp;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
